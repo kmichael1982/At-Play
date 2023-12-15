ef67a98f053a0b549cd7587f6b9297a3140dd37f
kmichael1982 <152282032+kmichael1982@users.noreply.github.com>
2023-12-15 16:48:07 +0400

    Update testimonailSlides.js

--- src/components/home/section/testimonial/testimonial-clients-list/testimonailSlides.js
+++ src/components/home/section/testimonial/testimonial-clients-list/testimonailSlides.js
@@ -5,33 +5,19 @@ import SThumbTwoImg from 'assets/images/home/testimonaials/s-thumb-two.png'
 export const testimonialSlides = [
     {
         imgSrc: SThumbTwoImg,
-        name: 'Daniel Smith',
-        description: 'posuere luctus orci. Donec vitae mattis quam, vitae tempor arcu. Aeneannon odio porttitor, convallis erat sit amet, facilisis velit. Nulla ornare convallis malesuada. Phasellus molestie, ipsum ac fringilla.',
-        role: 'Senior engineer'
+        name: 'Matt Anderson',
+        description: 'I can't say enough good things about Atplay! Working with them was an absolute game-changer for my business. Their creative marketing strategies are out-of-the-box and incredibly effective. From the moment we started collaborating, they took the time to understand my brand and objectives, and their team consistently delivered fresh, innovative ideas that made a significant impact on our online presence. Their attention to detail, professionalism, and ability to stay ahead of industry trends sets them apart. If you're looking for a marketing agency that will take your brand to the next level, Atplay is the one to go to.'
     },
     {
         imgSrc: SThumbThreeImg,
-        name: 'Daniel Smith',
-        description: 'posuere luctus orci. Donec vitae mattis quam, vitae tempor arcu. Aeneannon odio porttitor, convallis erat sit amet, facilisis velit. Nulla ornare convallis malesuada. Phasellus molestie, ipsum ac fringilla.',
-        role: 'Senior engineer'
+        name: 'David Chambers',
+        description: 'Atplay is simply outstanding! I've had the pleasure of working with them for the past year, and the results have been nothing short of remarkable. Their team's creative prowess is unmatched, and they have a knack for turning complex concepts into compelling marketing campaigns. They've helped me rebrand my business, develop a captivating website, and execute a social media strategy that has boosted engagement and sales. The level of dedication and passion they bring to their work is truly commendable. Atplay isn't just a marketing agency; they're strategic partners who genuinely care about your success.'
     },
     {
         imgSrc: SThumbImg,
-        name: 'Daniel Smith',
-        description: 'posuere luctus orci. Donec vitae mattis quam, vitae tempor arcu. Aeneannon odio porttitor, convallis erat sit amet, facilisis velit. Nulla ornare convallis malesuada. Phasellus molestie, ipsum ac fringilla.',
-        role: 'Senior engineer'
+        name: 'Sarah Mitchell',
+        description: 'Atplay is a breath of fresh air in the world of marketing agencies. After several disappointing experiences with other firms, I was hesitant to try again, but Atplay exceeded all my expectations. From the initial consultation to the ongoing campaign management, their team has been exceptional. They took the time to understand my niche industry and created a tailor-made marketing plan that generated immediate results. What sets Atplay apart is their commitment to transparency and open communication. They provide regular updates and reports, ensuring I'm always in the loop about the progress of our campaigns. If you're seeking a creative marketing agency that delivers on its promises, look no further than Atplay. They're the real deal!'
     },
-    {
-        imgSrc: SThumbThreeImg,
-        name: 'Daniel Smith',
-        description: 'posuere luctus orci. Donec vitae mattis quam, vitae tempor arcu. Aeneannon odio porttitor, convallis erat sit amet, facilisis velit. Nulla ornare convallis malesuada. Phasellus molestie, ipsum ac fringilla.',
-        role: 'Senior engineer'
-    },
-    {
-        imgSrc: SThumbImg,
-        name: 'Daniel Smith',
-        description: 'posuere luctus orci. Donec vitae mattis quam, vitae tempor arcu. Aeneannon odio porttitor, convallis erat sit amet, facilisis velit. Nulla ornare convallis malesuada. Phasellus molestie, ipsum ac fringilla.',
-        role: 'Senior engineer'
-    },
+    
 
 ]
