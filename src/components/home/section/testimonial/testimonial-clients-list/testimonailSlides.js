e42aa2db1b16784283655686d81b953865cced27
kmichael1982 <152282032+kmichael1982@users.noreply.github.com>
2023-12-15 18:44:57 +0400

    Update testimonailSlides.js

--- src/components/home/section/testimonial/testimonial-clients-list/testimonailSlides.js
+++ src/components/home/section/testimonial/testimonial-clients-list/testimonailSlides.js
@@ -1,22 +1,37 @@
 import SThumbImg from 'assets/images/home/testimonaials/s-thumb.png'
 import SThumbThreeImg from 'assets/images/home/testimonaials/s-thumb-three.png'
 import SThumbTwoImg from 'assets/images/home/testimonaials/s-thumb-two.png'
+
 export const testimonialSlides = [
     {
         imgSrc: SThumbTwoImg,
-        name: 'Matt Anderson',
-        description: 'I can't say enough good things about Atplay. Working with them was an absolute game-changer for my business. Their creative marketing strategies are out-of-the-box and incredibly effective.',
+        name: 'Daniel Smith',
+        description: 'posuere luctus orci. Donec vitae mattis quam, vitae tempor arcu. Aeneannon odio porttitor, convallis erat sit amet, facilisis velit. Nulla ornare convallis malesuada. Phasellus molestie, ipsum ac fringilla.',
+        role: 'Senior engineer'
     },
     {
         imgSrc: SThumbThreeImg,
-        name: 'David Chambers',
-        description: 'Atplay is simply outstanding. I've had the pleasure of working with them for the past year, and the results have been nothing short of remarkable.',
+        name: 'Daniel Smith',
+        description: 'posuere luctus orci. Donec vitae mattis quam, vitae tempor arcu. Aeneannon odio porttitor, convallis erat sit amet, facilisis velit. Nulla ornare convallis malesuada. Phasellus molestie, ipsum ac fringilla.',
+        role: 'Senior engineer'
     },
     {
         imgSrc: SThumbImg,
-        name: 'Sarah Mitchell',
-        description: 'Atplay is a breath of fresh air in the world of marketing agencies. After several disappointing experiences with other firms, I was hesitant to try again, but Atplay exceeded all my expectations.',
+        name: 'Daniel Smith',
+        description: 'posuere luctus orci. Donec vitae mattis quam, vitae tempor arcu. Aeneannon odio porttitor, convallis erat sit amet, facilisis velit. Nulla ornare convallis malesuada. Phasellus molestie, ipsum ac fringilla.',
+        role: 'Senior engineer'
     },
-    ]
+    {
+        imgSrc: SThumbThreeImg,
+        name: 'Daniel Smith',
+        description: 'posuere luctus orci. Donec vitae mattis quam, vitae tempor arcu. Aeneannon odio porttitor, convallis erat sit amet, facilisis velit. Nulla ornare convallis malesuada. Phasellus molestie, ipsum ac fringilla.',
+        role: 'Senior engineer'
+    },
+    {
+        imgSrc: SThumbImg,
+        name: 'Daniel Smith',
+        description: 'posuere luctus orci. Donec vitae mattis quam, vitae tempor arcu. Aeneannon odio porttitor, convallis erat sit amet, facilisis velit. Nulla ornare convallis malesuada. Phasellus molestie, ipsum ac fringilla.',
+        role: 'Senior engineer'
+    },
 
 ]
