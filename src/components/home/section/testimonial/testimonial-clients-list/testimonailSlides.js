4d24d6f8051556b48d59c671950d33ab98fb8576
kmichael1982 <152282032+kmichael1982@users.noreply.github.com>
2023-12-15 18:36:01 +0400

    Update testimonailSlides.js

--- src/components/home/section/testimonial/testimonial-clients-list/testimonailSlides.js
+++ src/components/home/section/testimonial/testimonial-clients-list/testimonailSlides.js
@@ -5,12 +5,12 @@ export const testimonialSlides = [
     {
         imgSrc: SThumbTwoImg,
         name: 'Matt Anderson',
-        description: 'I can't say enough good things about Atplay! Working with them was an absolute game-changer for my business. Their creative marketing strategies are out-of-the-box and incredibly effective.',
+        description: 'I can't say enough good things about Atplay. Working with them was an absolute game-changer for my business. Their creative marketing strategies are out-of-the-box and incredibly effective.',
     },
     {
         imgSrc: SThumbThreeImg,
         name: 'David Chambers',
-        description: 'Atplay is simply outstanding! I've had the pleasure of working with them for the past year, and the results have been nothing short of remarkable.',
+        description: 'Atplay is simply outstanding. I've had the pleasure of working with them for the past year, and the results have been nothing short of remarkable.',
     },
     {
         imgSrc: SThumbImg,
